04bfdc22cfed3659b3ca3bb72bfa180da959d276
Natnaree Chongsiriwattan <khawnat.c@gmail.com>
2020-11-11 15:44:21 +0700

    added entrance

---
+++ wearejack/entrance.py
@@ -0,0 +1,10 @@
+"""
+Wleocme to the world of fiction.
+
+
+"""
+
+
+def entrance():
+    """Enter the maze or not...."""
+    say"(Welcome to the world of fantacy, the story of Jack the giant warior.")
