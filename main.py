92370ca1baf9cb82122c68c541841cac60e0e884
Natnaree Chongsiriwattan <khawnat.c@gmail.com>
2020-11-11 14:22:22 +0700

    Added basic main

---
+++ main.py
@@ -0,0 +1,23 @@
+"""
+SHORT DESCRIPTION ON ONE LINE.
+
+LONG DESCRIPTION
+MORE THAN
+ONE LINE
+
+Authors: Natnaree Chongsiriwattana, Zijing Wu, Chollada Panbutr,
+ Pradchayaporn Nonthavanich
+
+License: AGPLv3 or later
+
+Usage:
+  main.py
+"""
+
+
+def main():
+    """Start the game."""
+
+
+if __name__ == "__main__":
+    main()
