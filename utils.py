2386adffec7938fce0951dbbc4cba2757d55ed29
Natnaree Chongsiriwattan <khawnat.c@gmail.com>
2020-11-18 13:52:51 +0700

    In Untility function: added askchoice and askword fuctions.

--- utils.py
+++ utils.py
@@ -4,3 +4,24 @@ import time
 def say(message):
     time.sleep(0.5)
     print(message)
+
+
+def askchoice(choices, message=""):
+    for idx, choice in enumerate(choices):
+        print(str(idx + 1) + ": " + choice)
+    while True:
+        answer = int(input(message))
+        if answer >= 1 and answer <= 4:
+            return answer
+        else:
+            print("Please select a correct choice(1-4)!")
+
+
+def askword(choices, message=""):
+    while True:
+        answer = input(message)
+        if answer in choices:
+            return answer
+        else:
+            print("Please enter the correct format!" + "(" + "/".join(choices)
+                     + ")")
