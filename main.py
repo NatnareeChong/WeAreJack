533b6d82bbb65924270a537846c189f251e43beb
Natnaree Chongsiriwattan <khawnat.c@gmail.com>
2020-11-18 14:23:33 +0700

    added more fun in main.py

--- main.py
+++ main.py
@@ -26,8 +26,38 @@ Usage:
 """
 
 
+from utils import say
+from stage1 import stage1
+import json
+
+
 def main():
     """Start the game."""
+    player = {}
+    player["hp"] = 3
+    player["items"] = {}
+    questions = loadquestion()
+    stage1_result = stage1(player, questions)
+    if not stage1_result:
+        return
+    question_left = 5
+    say("Congrats! You have passed 10 levels")
+    say("You have to answer 5 questions to kill the giant!")
+    weapons = []
+    for k, v in player["items"].items():
+        if k != "healing" and k not in weapons and v > 0:
+            weapons.append(k)
+        if len(weapons) == 4:
+            say("Since you have all weapons.",
+                "You have to answer only two questions to kill the giant!")
+            question_left = 2
+
+
+def loadquestion():
+    questions = None
+    with open("question.json", "r") as question:
+        questions = json.load(question)
+    return questions
 
 
 if __name__ == "__main__":
