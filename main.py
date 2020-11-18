02207821658f2afd4ff6495e8f3fff01f0dd436f
natthawu <natthawu@gymac>
2020-11-18 13:29:07 +0700

    Added discription on main.py

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
 """
-SHORT DESCRIPTION ON ONE LINE.
+Advanture into the JACK land!
 
-LONG DESCRIPTION
-MORE THAN
-ONE LINE
+You might have heard of the story Jack and the magic beans. Jack accidentally
+get a hold of the bean yesterday and planted them in his back garden.
 
-Authors: Natnaree Chongsiriwattana, Zijing Wu, Chollada Panbutr,
+He watered them day after day not knowing its magical property.
+
+As time passes the tiny beans grow into a giant bean sprout.
+
+Now the adventure in the giant land await him.
+
+For Jack to win and survive, we MUST to help Jack retrieves as much treasures
+and items to fight against the giants.
+
+Hope you enjoy the game!
+
+Authors: Natnaree Chongsiriwattana, Ziying Wu, Chollada Panbutr,
  Pradchayaporn Nonthavanich
 
 License: AGPLv3 or later
 
 Usage:
   main.py
+
 """
 
 
