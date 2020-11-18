21564efa5942694b5665238cbea37e7bee8e69f9
Natnaree Chongsiriwattan <khawnat.c@gmail.com>
2020-11-18 14:18:07 +0700

    added question

--- specialquestion.py
+++ specialquestion.py
@@ -1,6 +1,12 @@
 {
-    "1":{
-        "question":"What is your name?",
-        "choice":["Boss","Pat","Sandy","Gue"],
-        "answer":"Pat"
+    "1": {
+        "question": "What is your name?",
+        "choice": ["Boss", "Jack", "Chris", "Jig"],
+        "answer": "Jack"
     },
+    "2": {
+        "question": "2+5",
+        "choice": ["5", "6", "7", "8"],
+        "answer": "7"
+    }
+}
