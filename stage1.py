26c8889b26a3fb7667509fc9dfd70801d1ba90c3
Natnaree Chongsiriwattan <khawnat.c@gmail.com>
2020-11-25 13:43:08 +0700

    Rewording

--- stage1.py
+++ stage1.py
@@ -6,11 +6,11 @@ def stage1(player, questions):
     question_number = [i for i in range(1, 11)]
     for round in range(10):
         if player["hp"] == 0:
-            say("Game Over!")
+            say("NOOO~~ Game Over! T_T You had failed to help Jack survived.")
             return False
-        say("This is level " + str(round + 1))
+        say("This is level " + str(round + 1) + "The land of Giants")
         if player["hp"] == 1:
-            say("You have only 1 HP left")
+            say("You have only 1 HP left. Choose your next move wisely")
             if "healing" in player["items"] and player["items"]["healing"] > 0:
                 use = askword(["Y", "N"],
                               "You have healing potion in your item portal."
@@ -19,9 +19,12 @@ def stage1(player, questions):
                 if use == "Y":
                     player["items"]["healing"] -= 1
                     player["hp"] += 1
-                    say("Congrats! Your HP has been increased!")
+                    say("Congrats! Your HP has been increased!But make sure it"
+                        "does not decrease in the future T_T")
                 else:
-                    say("Try your best to survive! Good luck!!!")
+                    say("You didn't use the potion."
+                        "I am sure you have plans -^- "
+                        "Try your best to survive! Good luck!!!")
         q = random.randint(0, 1)
         if q:
             random_question = random.choice(question_number)
@@ -37,7 +40,8 @@ def stage1(player, questions):
                 player["hp"] -= 1
                 say("Wrong! Your HP is " + str(player["hp"]))
         else:
-            say("You have to choose left/right")
+            say("You have to choose left/right to get closer to the unknown_^_"
+                )
             direction = random.choice(["left", "right"])
             select = askword(["left", "right"], "(left/right): ")
             if direction == select:
@@ -49,6 +53,7 @@ def stage1(player, questions):
                     player["items"][player_get] += 1
                 say(player["items"])
             else:
-                say("You took the wrong turn, you can go to the next level")
+                say("You took the wrong turn, "
+                    "But lets move on to the next level")
     else:
         return True
--- stage2.py
+++ stage2.py
@@ -6,11 +6,11 @@ def stage2(player, special_question, question_left):
     question_number = [i for i in range(1, 6)]
     for round in range(question_left):
         if player["hp"] == 0:
-            say("Game Over!")
+            say("NOOO~~ Game Over! T_T You had failed to help Jack survived.")
             return False
         say("This is giant level " + str(round + 1))
         if player["hp"] == 1:
-            say("You have only 1 HP left")
+            say("You have only 1 HP left. Try your best to survived!")
             if "healing" in player["items"] and player["items"]["healing"] > 0:
                 use = askword(["Y", "N"],
                               "Do you want to use healing potion(Y/N): ")
@@ -25,12 +25,15 @@ def stage2(player, special_question, question_left):
         question = special_question[str(random_question)]
         solution = question["answer"]
         choices = question["choice"]
-        say(question["question"])
+        say(question["question"] + "Choose the right answer to get closer"
+            "to the treassure")
         answer = askchoice(choices, "Select a choice(1-4): ")
         if choices[answer - 1] == solution:
-            say("Correct! You can go to the next level")
+            say("Correct! You can go to the next level. Now you are another "
+                "step closer to kill the giants!")
         else:
             player["hp"] -= 1
-            say("Wrong! Your HP is " + str(player["hp"]))
+            say("Wrong! You are bleeding!! and Your HP is " + str(player["hp"])
+                + "Try to move faster and don't get caught by the giants")
     else:
         return True
